75eaca8bb2608f72942ef14a3af3003f6a082638
Michael Aubry <michaelaubry1991@gmail.com>
2014-11-29 20:17:40 -0600

    Fixed to the right database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     //'url' : 'mongodb://localhost/demoApp' // local
-    'url' : 'mongodb://stats.bluematter.io/statApp' // production
+    'url' : 'mongodb://bluematter.io/statApp' // production
 };
