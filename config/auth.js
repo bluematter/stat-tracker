5f6be6d90e567e262f06c67d10c71eb84c29a582
Michael Aubry <michaelaubry@Michaels-MBP-3.hackerdojo.com>
2015-11-29 12:21:06 -0800

    updated facebook callback url

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
         'clientID'      : '446186442184074', // your App ID
         'clientSecret'  : 'bcd77abd0fade8e9fb4da85b01bfdedf', // your App Secret
         //'callbackURL'   : 'http://localhost:4568/auth/facebook/callback' // local
-        'callbackURL'   : 'http://stats.bluematter.io/auth/facebook/callback' // production
+        'callbackURL'   : 'http://hardwoodstats.com/auth/facebook/callback' // production
     },
 
     'twitterAuth' : {
